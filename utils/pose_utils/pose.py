e67c0f124303c9a1740f1758e13cb58f071174db
Luiz Felipe de Camargo <camargo.luizfelipe@gmail.com>
2023-06-15 10:41:45 -0300

    Update pose.py

--- utils/pose_utils/pose.py
+++ utils/pose_utils/pose.py
@@ -325,7 +325,7 @@ class Pushup(Pose):
         if self.video_reader.is_opened() is False:
             print("Error File Not Found.")
 
-        out = cv2.VideoWriter("output.mp4", self.fourcc, self.video_fps, (self.width, self.height))
+        out = cv2.VideoWriter("output_pushup.mp4", self.fourcc, self.video_fps, (self.width, self.height))
         pushup_count_prev = pushup_count_current = progress_counter = 0
         progress_bar_color = (255, 255, 255)
         while self.video_reader.is_opened():
@@ -486,7 +486,7 @@ class Plank(Pose):
         time_adjustment = 6 / self.video_fps # 6 is magic number
         progress_counter = 0
         progress_bar_color = (255, 255, 255)
-        out = cv2.VideoWriter("output.mp4", self.fourcc, self.video_fps, (self.width, self.height))
+        out = cv2.VideoWriter("output_plank.mp4", self.fourcc, self.video_fps, (self.width, self.height))
         while self.video_reader.is_opened():
             image = self.video_reader.read_frame()
             if image is None:
@@ -552,7 +552,7 @@ class Squat(Pose):
         if self.video_reader.is_opened() is False:
             print("Error File Not Found.")
 
-        out = cv2.VideoWriter("output.mp4", self.fourcc, self.video_fps, (self.width, self.height))
+        out = cv2.VideoWriter("output_squat.mp4", self.fourcc, self.video_fps, (self.width, self.height))
         squads_count_prev = squad_count_current = progress_counter = 0
         progress_bar_color = (255, 255, 255)
         while self.video_reader.is_opened():
@@ -642,7 +642,7 @@ class Jumpingjack(Pose):
         if self.video_reader.is_opened() is False:
             print("Error File Not Found.")
 
-        out = cv2.VideoWriter("output.mp4", self.fourcc, self.video_fps, (self.width, self.height))
+        out = cv2.VideoWriter("output_jack.mp4", self.fourcc, self.video_fps, (self.width, self.height))
         jj_count_prev = jj_count_current = progress_counter = 0
         progress_bar_color = (255, 255, 255)
         while self.video_reader.is_opened():
